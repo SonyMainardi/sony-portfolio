2d1a26d5e61db8ad84867f558f3e9bdefa51e628
Sony Mainardi <141252102+SonyMainardi@users.noreply.github.com>
2023-12-26 14:57:37 -0300

    atualização json com dados pessoais.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/SonyMainardi/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/SonyMainardi/sony-portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
